1d856147f4244828f30cc3878a5deb497b26e959
Enzo Bozzani <bozzanienzo@gmail.com>
2024-08-30 14:50:04 -0300

    feat: add concurrency to upload data faster

--- load_images.py
+++ load_images.py
@@ -3,8 +3,16 @@ from fastcore.all import L  # type: ignore
 from fastai.vision.all import Path, download_images, resize_images, verify_images, get_image_files  # type: ignore # noqa: E501
 from time import sleep
 import json
+from concurrent.futures import ThreadPoolExecutor
+from threading import local
 
 ddgs = DDGS()
+thread_local = local()
+
+path = Path('./dogs')
+
+if not path.exists():
+    path.mkdir()
 
 
 def search_images(term, max_images=30):
@@ -12,29 +20,39 @@ def search_images(term, max_images=30):
     return L(ddgs.images(term, max_results=max_images)).itemgot('image')
 
 
-breeds: list[str] = []
-
-
-with open('./breeds.json') as file:
-    data = json.load(file)['message']
-
-    for key in data:
-        breedList: list[str] = data[key]
-
-        if len(breedList) != 0:
-            for breed in breedList:
-                breeds.append(f"{key} {breed}")
-        else:
-            breeds.append(key)
-
-path = Path('./dogs')
-
-for breed in breeds:
+def search_breed(breed):
     dest = (path/breed)
     dest.mkdir(exist_ok=True, parents=True)
-    download_images(dest, urls=search_images(f'{breed} dog photos'))
+    download_images(dest, urls=search_images(f'{breed} dog photos', 50))
     sleep(10)
     resize_images(path/breed, max_size=400, dest=path/breed)
     failed = verify_images(get_image_files(dest))
     failed.map(Path.unlink)
-    print((f"{len(failed)} failed"))
+    print((f"failed images for {breed}: {len(failed)}"))
+
+
+def search_all(breeds):
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        executor.map(search_breed, breeds)
+
+
+def main() -> None:
+    breeds: list[str] = []
+
+    with open('./breeds.json') as file:
+        data = json.load(file)['message']
+
+        for key in data:
+            breedList: list[str] = data[key]
+
+            if len(breedList) != 0:
+                for breed in breedList:
+                    breeds.append(f"{key} {breed}")
+            else:
+                breeds.append(key)
+
+    search_all(breeds)
+
+
+if __name__ == '__main__':
+    main()
--- model.py
+++ model.py
@@ -1,4 +1,5 @@
-from fastai.vision.all import Path, get_image_files, DataBlock, ImageBlock, CategoryBlock, RandomSplitter, parent_label, Resize, vision_learner, error_rate, resnet18, PILImage  # type: ignore  # noqa: E501
+# from fastai.vision.widgets import ImageClassifierCleaner  # type: ignore
+from fastai.vision.all import Path, get_image_files, DataBlock, ImageBlock, CategoryBlock, RandomSplitter, parent_label, Resize, vision_learner, error_rate, resnet18, PILImage, RandomResizedCrop, ClassificationInterpretation  # type: ignore  # noqa: E501
 import os
 
 
@@ -8,43 +9,65 @@ def list_breeds():
     return dirs
 
 
-path = Path('./dogs')
+def main() -> None:
+    if not os.path.exists('./dogs'):
+        print("Run 'python3 load_images.py' first!")
+        return
 
-dest = input('Image: ')
-# resize_image(path2/dest, max_size=400, dest=path2/dest)
+    path = Path('./dogs')
+
+    dest = input('Image: ')
+    # resize_image(path2/dest, max_size=400, dest=path2/dest)
+
+    data_blocks = DataBlock(
+        blocks=(ImageBlock, CategoryBlock),
+        get_items=get_image_files,
+        splitter=RandomSplitter(valid_pct=0.2, seed=42),
+        get_y=parent_label,
+        item_tfms=[Resize(192, method='squish')]
+    )
+
+    data_blocks = data_blocks.new(
+        item_tfms=RandomResizedCrop(128, min_scale=0.3)
+        # batch_tfms=aug_transforms()
+    )
+
+    data_loaders = data_blocks.dataloaders(path)
+
+    learn = vision_learner(data_loaders, resnet18, metrics=error_rate)
+    learn.fine_tune(3)
+
+    interp = ClassificationInterpretation.from_learner(learn)
+    print(interp.plot_confusion_matrix())
+
+    print(interp.plot_top_losses(5, nrows=1))
+
+    # cleaner = ImageClassifierCleaner(learn)
+    # print(cleaner)
+
+    available_breeds = list_breeds()
+
+    most_likely_breed,  _,  probs = learn.predict(PILImage.create(dest))
+    print(f"This is probably a: {most_likely_breed}.")
+
+    probabilities: list[float] = []
+
+    for prob in probs:
+        probabilities.append(prob)
+
+    ten_most_likely: list[tuple[str, float]] = []
+
+    for i in range(10):
+        prob = max(probabilities)
+        index = probabilities.index(prob)
+        probabilities.pop(index)
+        breed = available_breeds[index]
+
+        ten_most_likely.append((breed, prob))
+
+    for breed in ten_most_likely:
+        print(f'Probability of being a {breed[0]}: {breed[1] * 100:.2f} %')
 
 
-dls = DataBlock(
-    blocks=(ImageBlock, CategoryBlock),
-    get_items=get_image_files,
-    splitter=RandomSplitter(valid_pct=0.2, seed=42),
-    get_y=parent_label,
-    item_tfms=[Resize(192, method='squish')]
-).dataloaders(path, bs=32)
-
-
-learn = vision_learner(dls, resnet18, metrics=error_rate)
-learn.fine_tune(3)
-
-available_breeds = list_breeds()
-
-most_likely_breed,  _,  probs = learn.predict(PILImage.create(dest))
-print(f"This is probably a: {most_likely_breed}.")
-
-probabilities: list[float] = []
-
-for prob in probs:
-    probabilities.append(prob)
-
-ten_most_likely: list[tuple[str, float]] = []
-
-for i in range(10):
-    prob = max(probabilities)
-    index = probabilities.index(prob)
-    probabilities.pop(index)
-    breed = available_breeds[index]
-
-    ten_most_likely.append((breed, prob))
-
-for breed in ten_most_likely:
-    print(f'Probability of being a {breed[0]}: {breed[1] * 100:.2f} %')
+if __name__ == '__main__':
+    main()
